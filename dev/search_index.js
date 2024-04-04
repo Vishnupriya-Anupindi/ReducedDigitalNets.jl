b6314fa145e50570461373d2d6c32d0b2be21f02
Documenter.jl <documenter@juliadocs.github.io>
2024-04-04 15:21:58 +0000

    build based on 5a38f62

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = ReducedDigitalNets","category":"page"},{"location":"#ReducedDigitalNets","page":"Home","title":"ReducedDigitalNets","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for ReducedDigitalNets.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [ReducedDigitalNets]","category":"page"}]
+}
